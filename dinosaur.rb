39195f1c03872c9496ff931b169e29c12206fbc8
Richard Ford <richardford10@gmail.com>
2024-12-27 11:29:27 -0500

    add sha256

--- dinosaur.rb
+++ dinosaur.rb
@@ -3,7 +3,7 @@ class Dinosaur < Formula
   desc "Print a friendly ASCII dinosaur"
   homepage "https://github.com/richardford10/dinosaur"
   url "https://github.com/richardford10/dinosaur/archive/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_THE_SHA256_OF_TARBALL"
+  sha256 "37cd1315be3a19d68f0b029dd7d2baa6e3c6408a6c86abb607216c8ad06abc5a"
   license "MIT"
 
   def install
